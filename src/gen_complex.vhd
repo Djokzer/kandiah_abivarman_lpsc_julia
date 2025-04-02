0dc146df50bbd89b8abe7ff9bb353439e2da0c81
Djokzer <abivarmank@gmail.com>
2025-04-02 16:47:03 +0200

    start project

---
+++ src/gen_complex.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+entity gen_complex is
+    port (
+        clk   : in std_logic;
+        reset : in std_logic
+    );
+end entity gen_complex;
+
+architecture rtl of gen_complex is
+
+begin
+
+    
+
+end architecture;
---
+++ src/julia_compute.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+entity julia_compute is
+    port (
+        clk   : in std_logic;
+        reset : in std_logic
+        
+    );
+end entity julia_compute;
+
+architecture rtl of julia_compute is
+
+begin
+
+    
+
+end architecture;
