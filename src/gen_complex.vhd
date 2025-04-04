de42ce7d855f9daf7bfd6c7a84c8f3ccc0630c6c
Djokzer <abivarmank@gmail.com>
2025-04-04 14:47:24 +0200

    gen_complex first draft completed, need to do a testbench

--- src/gen_complex.vhd
+++ src/gen_complex.vhd
@@ -6,38 +6,43 @@ use ieee.math_real.all;
 entity gen_complex_coord is
 	generic (
 		-- SCREEN SIZE
-		X_SIZE	: integer := 10;
-		Y_SIZE 	: integer := 10;
+		X_SIZE		: integer := 10;
+		Y_SIZE 		: integer := 10;
 
 		-- COMPLEX PLANE
-		X_RE_MIN: integer := -2;
-		X_IM_MIN: integer := 2;
-		Y_RE_MIN: integer := -2;
-		Y_IM_MIN: integer := 2
+		RE_MIN		: integer := -2;
+		RE_MAX		: integer := 2;
+		IM_MIN		: integer := -2;
+		IM_MAX		: integer := 2;
+
+		-- FIXED POINT
+		FIXED_BITS	: integer := 20
 	);
 	port (
 		-- GENERAL
-		clk		: in std_logic;
-		rst		: in std_logic;
+		clk			: in std_logic;
+		rst			: in std_logic;
 
 		-- COMPLEX
-		x_re    : in std_logic_vector(31 downto 0);
-		x_im    : in std_logic_vector(31 downto 0);
-		y_re    : in std_logic_vector(31 downto 0);
-		y_im    : in std_logic_vector(31 downto 0)
+		c_re    	: out std_logic_vector(31 downto 0);
+		c_im    	: out std_logic_vector(31 downto 0)
 	);
 end entity gen_complex_coord;
 
 architecture rtl of gen_complex_coord is
 
+	-- COMPLEX STEP CONSTANTS
+	constant RE_RANGE	: integer := RE_MAX - RE_MIN;
+	constant IM_RANGE	: integer := IM_MAX - IM_MIN;
+
+	constant STEP_HORI : integer := integer(RE_RANGE * 2**FIXED_BITS / X_SIZE-1);
+	constant STEP_VERT : integer := integer(IM_RANGE * 2**FIXED_BITS / Y_SIZE-1);
+	
 	-- COORDINATES COUNTER
 	signal x_coord : integer := 0;
 	signal y_coord : integer := 0;
 
-
-
 begin
-
 	-- COUNT X COORDINATE FROM 0 TO X_SIZE-1
 	x_counter : process(clk, rst)
 	begin
@@ -69,6 +74,18 @@ begin
 				end if;
 			end if;
 		end if;
+	end process;
+
+	-- CONVERT X,Y COORDINATES TO COMPLEX PLANE
+	compute_complex : process(x_coord, y_coord)
+		variable real_val : integer;
+		variable imag_val : integer;
+	begin
+		real_val := X_SIZE + x_coord * STEP_HORI;
+		imag_val := Y_SIZE + y_coord * STEP_VERT;
+
+		c_re <= std_logic_vector(to_signed(real_val, c_re'length));
+		c_im <= std_logic_vector(to_signed(imag_val, c_im'length));
 	end process;
 
 end architecture;
