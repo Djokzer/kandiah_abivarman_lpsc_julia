cbcaf175040886170a367ffe8129053d6118ce29
Djokzer <abivarmank@gmail.com>
2025-04-25 16:06:33 +0200

    finished julia compute, need to test it with testbench

--- src/julia_compute.vhd
+++ src/julia_compute.vhd
@@ -18,6 +18,10 @@ entity julia_compute is
 		-- FIXED POINT  
 		FIXED_BITS		: integer := 20;
 
+		-- JULIA ESCAPE ALGORITHM ARGS
+		ITERATIONS		: integer := 100;
+		THRESHOLD		: integer := 2;
+
 		-- COMPLEX CONST
 		c_re			: integer := 298844;    -- Real = 0.285, with Fixed bits = 0.285 * 2**20
 		c_im			: integer := 10485      -- Imaginary = 0.01j, with Fixed bits = 0.01 * 2**20
@@ -35,6 +39,9 @@ entity julia_compute is
 end entity julia_compute;
 
 architecture rtl of julia_compute is
+	-- CONSTANTS
+	constant FIXED_THRESHOLD : integer := (THRESHOLD**2) * 2**FIXED_BITS;
+
 	-- X,Y COORDINATES COUNTER
 	signal x_coord			: std_logic_vector(15 downto 0);
 	signal y_coord			: std_logic_vector(15 downto 0);
@@ -46,11 +53,18 @@ architecture rtl of julia_compute is
 	signal z_im				: std_logic_vector(31 downto 0);
 	
 	-- JULIA COMPUTING SIGNALS
+	signal julia_count	 	: integer := 0;
+	signal julia_enable		: std_logic := '0';
+	signal julia_init		: std_logic := '0';
+
 	signal z_re_n, z_im_n	: integer := 0; -- JULIA ITER INPUT
+	signal z_re_n1, z_im_n1	: integer := 0; -- JULIA ITER OUTPUT
+	signal z_norm 			: integer := 0; -- COMPLEX NORM, TO CHECK STOP CONDITION
 	
 	-- FSM STATES
-	type FSM_STATE_TYPE is (JULIA_COMP, JULIA_OUTPUT, COUNT_COORD);
+	type FSM_STATE_TYPE is (JULIA_INI, JULIA_COMP, JULIA_OUTPUT, COUNT_COORD);
 	signal curr_fsm_state, next_fsm_state : FSM_STATE_TYPE := JULIA_COMP;
+	signal data_valid : std_logic := '0';
 
 begin
 	-- ==============  COMPLEX COORDINATE GENERATOR =============== --
@@ -73,26 +87,48 @@ begin
 	-- ============================================================ --
 
 	-- ============================ FSM =========================== --
-	fsm_ctrl : process(curr_fsm_state)
+	fsm_ctrl : process(curr_fsm_state, julia_count, z_norm)
 	begin
 		-- DEFAULTS
-		next_fsm_state <= curr_fsm_state;
+		next_fsm_state 	<= curr_fsm_state;
+		count_enable 	<= '0';
+		julia_enable 	<= '0';
+		julia_init		<= '0';
+		data_valid		<= '0';
 
 		case curr_fsm_state is
+			when JULIA_INI =>
+				julia_init <= '1';
+				next_fsm_state <= JULIA_COMP;
+
 			when JULIA_COMP =>
+				julia_enable <= '1';
+				if julia_count < ITERATIONS then
+					if z_norm > FIXED_THRESHOLD then
+						-- Z ESCAPED
+						next_fsm_state <= JULIA_OUTPUT;
+					end if;
+				else
+					-- Z IS IN JULIA SET
+					next_fsm_state <= JULIA_OUTPUT;
+				end if;
 
 			when JULIA_OUTPUT =>
-
+				-- DATA OUT VALID
+				data_valid <= '1';
+				next_fsm_state <= COUNT_COORD;
 			when COUNT_COORD =>
+				-- ENABLE X,Y COUNTER
+				count_enable <= '1';
+				next_fsm_state <= JULIA_INI;
 		end case;
-	
 	end process;
 
 	fsm_update : process(clk, rst)
 	begin
 		if rising_edge(clk) then
 			if rst = '1' then
-				curr_fsm_state <= JULIA_COMP;
+				curr_fsm_state <= JULIA_INI;
 			else
 				curr_fsm_state <= next_fsm_state;
 			end if;
@@ -101,9 +137,41 @@ begin
 	-- ============================================================ --
 
 	-- =============== JULIA ITERATIONS COMPUTATION =============== --
-	julia_iter : process(z_re_n, z_im_n)
+	julia_counter : process(clk, rst)
 	begin
-		
+		if rising_edge(clk) then
+			if rst = '1' then
+				z_re_n <= 0;
+				z_im_n <= 0;
+				julia_count <= 0;
+			else
+				if julia_enable = '1' then
+					-- UPDATE Z and COUNTER
+					z_re_n <= z_re_n1;
+					z_im_n <= z_im_n1;
+					julia_count <= julia_count + 1;
+				end if;
+
+				if julia_init = '1' then
+					-- INIT Z and COUNTER
+					z_re_n <= to_integer(signed(z_re));
+					z_im_n <= to_integer(signed(z_im));
+					julia_count <= 0;
+				end if;
+			end if;
+		end if;
+	end process;
+
+	julia_iter : process(z_re_n, z_im_n)
+		variable z_re_sq, z_im_sq, xy : integer;
+	begin
+		z_re_sq := to_integer(shift_right(to_signed(z_re_n * z_re_n, 32), FIXED_BITS));		-- z_re^2
+		z_im_sq := to_integer(shift_right(to_signed(z_im_n * z_im_n, 32), FIXED_BITS));		-- z_im^2
+		xy 		:= to_integer(shift_right(to_signed(z_re_n * z_im_n, 32), FIXED_BITS - 1)); -- 2 * z_re * z_im
+
+		z_re_n1 <= z_re_sq - z_im_sq + c_re;	-- z_re_n+1 = z_re^2 - z_im^2 + C_RE 
+		z_im_n1 <= xy + c_im;					-- z_im_n+1 = 2 * z_re * z_im + C_IM
+		z_norm	<= z_re_sq + z_im_sq;			-- z_re^2 + z_im^2
 	end process;
 	-- ============================================================ --
 
@@ -151,6 +219,9 @@ begin
 
 	-- ============================================================ --
 
-
-
+	-- ========================== OUTPUT ========================== --
+	escape_counter <= std_logic_vector(to_unsigned(julia_count, escape_counter'length));
+	address <= std_logic_vector(to_unsigned(x_count + y_count * X_SIZE, address'length));
+	valid <= data_valid;
+	-- ============================================================ --
 end architecture;
---
+++ src/test/julia_compute_tb.vhd
@@ -0,0 +1,90 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library std;
+use std.textio.all;
+use std.env.finish;
+
+entity julia_compute_tb is
+end;
+
+architecture testbench of julia_compute_tb is
+	-- CONSTANTS
+	constant clk_period : time := 5 ns;
+
+	-- Generics
+	constant X_SIZE      : integer := 100;
+	constant Y_SIZE      : integer := 100;
+	constant RE_MIN      : integer := -2;
+	constant RE_MAX      : integer := 2;
+	constant IM_MIN      : integer := -2;
+	constant IM_MAX      : integer := 2;
+	constant FIXED_BITS  : integer := 20;
+	constant ITERATIONS  : integer := 100;
+	constant THRESHOLD   : integer := 2;
+	constant C_RE_FIXED  : integer := integer(0.285 * 2.0**FIXED_BITS);
+	constant C_IM_FIXED  : integer := integer(0.01 * 2.0**FIXED_BITS);
+
+	-- SIGNALS
+	signal clk            : std_logic := '0';
+	signal rst            : std_logic;
+
+	signal escape_counter : std_logic_vector(7 downto 0);
+	signal address        : std_logic_vector(31 downto 0);
+	signal valid          : std_logic;
+
+	-- CONTROL SIGNAL
+	signal finished       : boolean := false;
+
+	-- FILE OUTPUT
+	file output_file : text open write_mode is "julia_output.csv";
+
+begin
+	-- CLOCK AND RESET CONTROL
+	clk <= not clk after clk_period / 2;
+	rst <= '1', '0'  after CLK_PERIOD * 10;
+
+	-- UNIT UNDER TEST
+	uut : entity work.julia_compute
+	generic map (
+		X_SIZE     => X_SIZE,
+		Y_SIZE     => Y_SIZE,
+		RE_MIN     => RE_MIN,
+		RE_MAX     => RE_MAX,
+		IM_MIN     => IM_MIN,
+		IM_MAX     => IM_MAX,
+		FIXED_BITS => FIXED_BITS,
+		ITERATIONS => ITERATIONS,
+		THRESHOLD  => THRESHOLD,
+		c_re       => C_RE_FIXED,
+		c_im       => C_IM_FIXED
+	)
+	port map (
+		clk            => clk,
+		rst            => rst,
+		escape_counter => escape_counter,
+		address        => address,
+		valid          => valid
+	);
+
+	-- CAPTURE AND FILE OUTPUT
+	write_output : process(clk)
+		variable L      : line;
+		variable val : integer;
+	begin
+		if rising_edge(clk) then
+			if rst = '0' and valid = '1' then
+				val := to_integer(unsigned(escape_counter));
+				write(L, val);
+				writeline(output_file, L);
+
+				if address = std_logic_vector(to_unsigned(X_SIZE * Y_SIZE - 1, 32)) then
+					report "-- All pixels written --";
+					wait for clk_period * 10;
+					finish;
+				end if;
+			end if;
+		end if;
+	end process;
+end architecture;
